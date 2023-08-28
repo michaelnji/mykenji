a5086c8a971193b3b50fe0062b59ebd70f022281
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-08-28 09:29:59 +0200

    feat: :wrench: Add custom css colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
 				sans: ['Epilogue'],
 				head: ['Krona One']
+			},
+			colors: {
+				'base-100': '#1f2937',
+				'base-200': '#111827',
+				'base-300': '#0a0d11',
+				'base-400': '#000814',
+				primary: '#f71735',
+				secondary: '#ff9f1c'
 			}
 		}
 	},
