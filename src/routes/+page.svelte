508c50d170f836a021411d6968f4706637745a75
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-08 14:40:13 +0100

    style: 💄 ui changes & added contact sectin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,20 @@
 	import Footer from '$lib/components/section/footer.svelte';
 	import Hero from '$lib/components/section/hero.svelte';
 	import Projects from '$lib/components/section/projects.svelte';
+	import Contact from '$lib/components/section/contact.svelte';
 </script>
 
 <div class="mb-24">
 <Hero />
 </div>
-	<div class="mt-44 mb-24">
+	<div class="mb-24 mt-32">
 		<About />
 	</div>
 <div class="my-24">
 	<Projects />
+</div>
+<div class="my-24">
+	<Contact />
 </div>
 <Footer />
 
