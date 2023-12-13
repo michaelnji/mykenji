5b300103b454ff58e42500fa6e67b89b89e17236
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-13 06:38:49 +0100

    feat: 💄 UI improvements/changes

--- src/lib/components/section/contact.svelte
+++ src/lib/components/section/contact.svelte
@@ -6,20 +6,49 @@
 	Contact Me
 </h1>
 
-<div class="flex w-full justify-center gap-6 mt-6">
-	<a href="https://discordapp.com/users/1086901938127388702" target="_blank" rel="noopener noreferrer"  class="hover:opacity-75 transition-opacity duration-200">
-	<Icon name="discord" color="#748ffc" height="68" width="58" />
+<div class="flex w-full justify-center gap-3 mt-6">
+	<a
+		href="https://discordapp.com/users/1086901938127388702"
+		target="_blank"
+		rel="noopener noreferrer"
+		class="hover:opacity-75 transition-opacity duration-200"
+	>
+		<div
+			class="py-2 px-4 rounded-xl flex gap-x-2 font-head bg-[#748ffc] bg-opacity-10 text-[#748ffc] border-2 border-[#748ffc] hover:rotate-3 hover:scale-x-90 transition-transform duration-500"
+		>
+			<Icon name="discord" color="#748ffc" />
+		</div>
 	</a>
-	<a href="https://twitter.com/cod3v1l" target="_blank" rel="noopener noreferrer" class="hover:opacity-75 transition-opacity duration-200">
-		<Icon name="twitter" color="#339af0" height="68" width="58" />
+	<a
+		href="https://twitter.com/cod3v1l"
+		target="_blank"
+		rel="noopener noreferrer"
+		class="hover:opacity-75 transition-opacity duration-200"
+	>
+		<div
+			class="py-2 px-4 rounded-xl flex gap-x-2 font-head bg-[#339af0] bg-opacity-10 text-[#339af0] border-2 border-[#339af0] hover:rotate-3 hover:scale-x-90 transition-transform duration-500"
+		>
+			<Icon name="twitter" color="#339af0" />
+		</div>
 	</a>
 	<a href="mailto:michaelnji34@gmail.com" class="hover:opacity-75 transition-opacity duration-200">
-		<Icon name="gmail" color="#ff6b6b" height="68" width="58" />
+		<div
+			class="py-2 px-4 rounded-xl flex gap-x-2 font-head bg-[#ff6b6b] bg-opacity-10 text-[#ff6b6b] border-2 border-[#ff6b6b] hover:rotate-3 hover:scale-x-90 transition-transform duration-500"
+		>
+			<Icon name="gmail" color="#ff6b6b" />
+		</div>
 	</a>
-	<a href="https://github.com/michaelnji" target="_blank" rel="noopener noreferrer" class="dark:hidden hover:opacity-75 transition-opacity duration-200">
-	<Icon name="github" color="#000" height="68" width="58"  />
-	</a>
-	<a href="https://github.com/michaelnji" target="_blank" rel="noopener noreferrer" class="hidden dark:inline-flex hover:opacity-75 transition-opacity duration-200">
-		<Icon name="github" color="#fff" height="68" width="58"  />
+	
+	<a
+		href="https://github.com/michaelnji"
+		target="_blank"
+		rel="noopener noreferrer"
+		class=" hover:opacity-75 transition-opacity duration-200"
+	>
+		<div
+			class="py-2 px-4 rounded-xl flex gap-x-2 font-head bg-[black] dark:bg-gray-100 text-black bg-opacity-10 border-2 border-[black] hover:rotate-3 hover:scale-x-90 transition-transform duration-500"
+		>
+			<Icon name="github" color="black" /> 
+		</div>
 	</a>
 </div>
