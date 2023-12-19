a706dfbc55ebaaaa0a17e416d5d6655bfcc9274d
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-19 21:15:32 +0100

    fix: 💄 Fixed colors on dark mode

--- src/lib/components/misc/blogAuthorInfo.svelte
+++ src/lib/components/misc/blogAuthorInfo.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="w-full flex items-center justify-center">
-    <div class="w-full hidden md:inline-flex  border-t-2 border-dashed"></div>
-    <div class="border-dashed border-2  p-3 w-full md:w-[80rem]">
+    <div class="w-full hidden md:inline-flex dark:border-gray-700  border-t-2 border-dashed"></div>
+    <div class="border-dashed border-2 dark:border-gray-700  p-3 w-full md:w-[80rem]">
     <div class=" flex flex-wrap gap-3 items-center ">
 				<img
 					src={author.imageUrl}
@@ -16,11 +16,11 @@
                     <p class="font-semibold"> {author.name}</p>
                     <a href={author.twitter} target="_blank"><p class="font-medium font-serif flex gap-2 opacity-70"> <Icon name='twitter'/>@cod3v1l</p></a>
                 </div>
-                <p class="mt-2 prose text-opacity-75">
+                <p class="mt-2 prose text-opacity-75 dark:prose-invert">
                     {author.bio[0].children[0].text}
                 </p>
 			</div>
             
         </div>
-        <div class="w-full hidden md:inline-flex  border-t-2 border-dashed"></div>
+        <div class="w-full hidden md:inline-flex  border-t-2 border-dashed dark:border-gray-700"></div>
 </div>
