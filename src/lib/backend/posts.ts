006b8dcf97d5c223281ac8f8bf0235098eb6cd3c
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-21 22:43:43 +0100

    feat: ✨ Refactor blog-related backend and UI components
    Introduce new function to retrieve related posts and add related posts UI component. Adapt blog page to display related posts. Enhances user engagement and navigation. Fixes #37.

--- src/lib/backend/posts.ts
+++ src/lib/backend/posts.ts
@@ -24,6 +24,27 @@ export async function getPostsSummarized() {
 		}
    }
 }
+export async function getRelatedPosts(slug:string|undefined ) {
+    const query = `*[_type == "post" && slug.current != "${slug}"] | order(publishedAt desc, _updatedAt desc) | order( _updatedAt desc)[0..1]{_id, "authorInfo":author-> {slug,name,bio,twitter,"imageUrl":image.asset->url}, "tags":categories[]->{title,description},excerpt, publishedAt, slug, title, _updatedAt,"imageUrl": mainImage.asset->url}`;
+   try {
+	 const data = await sanityClient.fetch(query);
+		if (data) {
+			return {
+				
+				posts: data
+			};
+		}
+		return {
+			status: 500,
+			body: new Error('Internal Server error')
+		};
+   } catch (error) {
+	return {
+			status: 500,
+			body: new Error('Internal Server error')
+		}
+   }
+}
 export async function getPost(slug:string|undefined ) {
     const query = `*[_type == "post" && slug.current == "${slug}"]{_id,body, "authorInfo":author-> {slug,name,bio,twitter,"imageUrl":image.asset->url}, "tags":categories[]->{title,description}, publishedAt, slug, title, _updatedAt,"imageUrl": mainImage.asset->url, "tableOfContents": content[style in ["h2", "h3"]]}`;
    try {
